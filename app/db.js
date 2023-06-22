97a9afb1b30aa612f1470a3d73f0008d861b7b19
Rohan Kumar Singh <73416648+Rohan-Kumar-Singh@users.noreply.github.com>
2023-06-22 11:22:13 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Root@2404',
+  password: 'rootpswd',
   database: 'twitter_clone',
 });
 
